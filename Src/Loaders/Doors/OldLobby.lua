91fa6970fe903873bbcdafa99e4b90ed279f3b5b
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-11-11 20:48:15 -0300

    Update OldLobby.lua

--- Src/Loaders/Doors/OldLobby.lua
+++ Src/Loaders/Doors/OldLobby.lua
@@ -1,8 +1,21 @@
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
-local Window = OrionLib:MakeWindow({Name = "Teleport Menu", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTeleport"})
+local OrionLib = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Giangplay/Script/main/Orion_Library_PE_V2.lua'))()
+local Window = OrionLib:MakeWindow({IntroText = "Msdoors | V1 ",Icon = "rbxassetid://133997875469993", IntroIcon = "rbxassetid://133997875469993", Name = "MsDoors | PRE HOTEL Lobby", HidePremium = false, SaveConfig = true, ConfigFolder = ".msdoors/places/Oldlobby"})
 
+--[[ APIS ]]--
+local MsdoorsNotify = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/Notification-doorsAPI/refs/heads/main/Msdoors/MsdoorsApi.lua"))()
+
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://4590656842"
+sound.Volume = 2
+sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+sound:Play()
+sound.Ended:Connect(function()
+    sound:Destroy()
+end)
+MsdoorsNotify("Msdoors", "Sistema carregado com sucesso! ", "Execução", "rbxassetid://130949777442519", Color3.new(128, 0, 128), 6)
+p
 local MSoldLobby = Window:MakeTab({
-	Name = "",
+	Name = "Funções",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
