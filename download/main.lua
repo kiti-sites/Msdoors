ce378e27a890f521e143f6eef82aa3cc58e36894
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-11-15 12:36:47 -0300

    Update main.lua

--- download/main.lua
+++ download/main.lua
@@ -1,4 +1,4 @@
-local url = "https://github.com/Sc-Rhyan57/Msdoors/releases/download/MsdoorsLoad/Msdoors.lua"
+local url = "https://github.com/Sc-Rhyan57/Msdoors/releases/download/Msdoors/msdoors-download.lua"
 local response = game:HttpGet(url, true)
 local script = loadstring(response)
 script()
