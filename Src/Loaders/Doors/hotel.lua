d52e7004baf78c74026f735f19416527051f1ede
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-11-15 15:47:55 -0300

    Update hotel.lua

--- Src/Loaders/Doors/hotel.lua
+++ Src/Loaders/Doors/hotel.lua
@@ -13,19 +13,7 @@ MsdoorsNotify("Msdoors","Por favor, execute no jogo não no lobby!","Execução"
 end
 
 
-MsdoorsNotify("Atualização","Você está usando uma versão desatualizada do Msdoors!","Nova versão disponível no github","rbxassetid://133997875469993", Color3.new(255, 0, 0), 15)
-local Oldversion = Window:MakeTab({
-    Name = "Atualizar Script",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
 
-Oldversion:AddButton({
-    Name = "Atualizar",
-    Callback = function()
-        OrionLib:Destroy() loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/Msdoors/refs/heads/main/download/main.lua"))()
-    end
-})
 --// Serviços \\--
 local Lighting = game:GetService("Lighting")
 local Players = game:GetService("Players")
