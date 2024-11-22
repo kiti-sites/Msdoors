2d2f4d958dd141f82194b053707f5d803992a6c9
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-11-21 21:14:49 -0300

    Update main.lua

--- download/main.lua
+++ download/main.lua
@@ -2,3 +2,24 @@ local url = "https://github.com/Sc-Rhyan57/Msdoors/releases/download/Msdoors/msd
 local response = game:HttpGet(url, true)
 local script = loadstring(response)
 script()
+
+if not makefolder then
+    error("[Msdoors] Seu executor não suporta manipulação de arquivos locais (makefolder).")
+end
+
+local baseFolder = ".msdoors"
+local placesFolder = baseFolder .. "/places"
+local presetsFolder = placesFolder .. "/presets"
+
+if not isfolder(baseFolder) then
+    makefolder(baseFolder)
+end
+
+if not isfolder(placesFolder) then
+    makefolder(placesFolder)
+end
+
+if not isfolder(presetsFolder) then
+    makefolder(presetsFolder)
+end
+print("[msdoors] A pasta foi criada com sucesso em:", presetsFolder)
