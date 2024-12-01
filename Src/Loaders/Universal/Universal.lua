f62fbd81a979ea939dd7d4004e67f924bc908a67
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-12-01 17:36:38 -0300

    Update Universal.lua

--- Src/Loaders/Universal/Universal.lua
+++ Src/Loaders/Universal/Universal.lua
@@ -1,1 +1,36 @@
+--// <UNIVERSAL> | UPDATE EM BREVEL \\--
+--[[
+                                                                                                                     
+     ______  _______            ______       _____           _____            _____         _____            ______  
+    |      \/       \       ___|\     \  ___|\    \     ____|\    \      ____|\    \    ___|\    \       ___|\     \ 
+   /          /\     \     |    |\     \|    |\    \   /     /\    \    /     /\    \  |    |\    \     |    |\     \
+  /     /\   / /\     |    |    |/____/||    | |    | /     /  \    \  /     /  \    \ |    | |    |    |    |/____/|
+ /     /\ \_/ / /    /| ___|    \|   | ||    | |    ||     |    |    ||     |    |    ||    |/____/  ___|    \|   | |
+|     |  \|_|/ /    / ||    \    \___|/ |    | |    ||     |    |    ||     |    |    ||    |\    \ |    \    \___|/ 
+|     |       |    |  ||    |\     \    |    | |    ||\     \  /    /||\     \  /    /||    | |    ||    |\     \    
+|\____\       |____|  /|\ ___\|_____|   |____|/____/|| \_____\/____/ || \_____\/____/ ||____| |____||\ ___\|_____|   
+| |    |      |    | / | |    |     |   |    /    | | \ |    ||    | / \ |    ||    | /|    | |    || |    |     |   
+ \|____|      |____|/   \|____|_____|   |____|____|/   \|____||____|/   \|____||____|/ |____| |____| \|____|_____|   
+    \(          )/         \(    )/       \(    )/        \(    )/         \(    )/      \(     )/      \(    )/     
+     '          '           '    '         '    '          '    '           '    '        '     '        '    '      
+                                                                                                                     
+                                        Por Rhyan57 💜
+  ]]--
+
+
+
+
+
+
+
+local OrionLib = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Giangplay/Script/main/Orion_Library_PE_V2.lua'))()
+local Window = OrionLib:MakeWindow({IntroText = "Msdoors | V1",Icon = "rbxassetid://133997875469993", IntroIcon = "rbxassetid://133997875469993", Name = "MsDoors", HidePremium = false, SaveConfig = true, ConfigFolder = ".msdoors/places/hotel"})
+--// APIS \\--
+--[[ MSDOORS API ]]--
+local MsdoorsNotify = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/Notification-doorsAPI/refs/heads/main/Msdoors/MsdoorsApi.lua"))()
+--[[ MS ESP(@mstudio45) - thanks for the API! ]]--
+local ESPLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/MS-ESP/refs/heads/main/source.lua"))()
+---[[ ELEMENTOS ]]--
+
+MsdoorsNotify("Msdoors","Msdoors foi carregado com sucesso!","Execução","rbxassetid://133997875469993", Color3.new(0.5, 0, 0.5), 5)
 
