8057749ac57e9cce194e6e62ec085b20a020e844
MathRapha <bissaroraphael@gmail.com>
2024-12-05 22:47:55 -0300

    Update customName.lua

--- Src/Loaders/Doors/test/customName.lua
+++ Src/Loaders/Doors/test/customName.lua
@@ -1,16 +1,14 @@
 local plr = game.Players.LocalPlayer
 local gui = plr.PlayerGui
 local name = gui:FindFirstChild("NameUI"..plr.Name)
+local ts = game:GetService("TweenService")
 local rs = game:GetService("RunService")
-local ng = require(107216037571494)()
 if name then
   local stuff = name.Stuff.Frame
-  ng.Parent = name.Username
   rs.RenderStepped:Connect(function ()
       name.Username.Text = "MS "..plr.Name
       stuff.TextBadge.Text = "MS Doors"
-      stuff.TextBadge.TextColor3 = Color3.fromRGB(0, 150 ,255)
-      ng.Rotation += 0.2
+      stuff.TextBadge.TextColor3 = Color3.fromRGB(0, 150, 255)
   end)
 else
   warn("Name UI of player "..plr.." not found!")
