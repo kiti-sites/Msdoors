4fcb8dd3bc5bd1d09b2f47781632496f12f30202
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-12-13 00:54:15 -0300

    Update Ui.lua

--- system/Ui.lua
+++ system/Ui.lua
@@ -9,12 +9,20 @@ local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
 Library.ShowCustomCursor = true
-Library.NotifySide = "Right"
+Library.NotifySide = "Left"
 
+local Window = Library:CreateWindow({
+    Title = 'Msdoors v1 | (BETA)',
+    Center = true,
+    AutoShow = true,
+})
 
 ThemeManager:SetLibrary(Library)
 SaveManager:SetLibrary(Library)
 SaveManager:SetFolder('msdoors')
+
+SaveManager:BuildConfigSection(Window:AddTab('Configurações'))
+ThemeManager:ApplyToTab(Window:AddTab('Temas'))
 
 getgenv().ActiveUI = {
     Library = Library,
