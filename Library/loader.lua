b549e18311dd03bf32d66fe1f63213abcecc91e7
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-12-12 21:54:05 -0300

    Update loader.lua

--- Library/loader.lua
+++ Library/loader.lua
@@ -1,1 +1,31 @@
+local repo = 'https://raw.githubusercontent.com/usuario/repositorio/main/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+
+-- Configurações básicas
+Library.ShowCustomCursor = true
+Library.NotifySide = "Left"
+
+local Window = Library:CreateWindow({
+    Title = 'Msdoors v1 | (BETA)',
+    Center = true,
+    AutoShow = true,
+})
+
+-- Configurações de tema e salvamento
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+SaveManager:SetFolder('msdoors')
+
+-- Aplica tema e configurações
+SaveManager:BuildConfigSection(Window:AddTab('Configurações'))
+ThemeManager:ApplyToTab(Window:AddTab('Temas'))
+
+return {
+    Library = Library,
+    ThemeManager = ThemeManager,
+    SaveManager = SaveManager
+}
 
