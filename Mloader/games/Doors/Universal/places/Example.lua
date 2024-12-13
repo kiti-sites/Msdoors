91a6a8f01df925292cc687503b9c7c3e4db18dd9
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-12-12 21:41:33 -0300

    Update Example.lua

--- Mloader/games/Doors/Universal/places/Example.lua
+++ Mloader/games/Doors/Universal/places/Example.lua
@@ -1,8 +1,4 @@
--- New example script written by wally
--- You can suggest changes with a pull request or something
-
 local repo = 'https://raw.githubusercontent.com/mstudio45/LinoriaLib/main/'
-
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
