fea5efe9abbddc0650d7b8931e16ff64f9f6629a
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-12-12 22:18:44 -0300

    Update Utils.lua

--- Library/shared/Utils.lua
+++ Library/shared/Utils.lua
@@ -1,1 +1,25 @@
+local Utils = {}
 
+function Utils:HttpGet(url)
+    local success, result = pcall(function()
+        return game:HttpGet(url)
+    end)
+
+    if not success then
+        warn("[Utils] Falha ao baixar conteúdo de " .. url .. ": " .. result)
+        return nil
+    end
+    return result
+end
+
+function Utils:SafeLoadString(content)
+    local success, err = pcall(function()
+        loadstring(content)()
+    end)
+
+    if not success then
+        warn("[Utils] Erro ao executar script: " .. err)
+    end
+end
+
+return Utils
