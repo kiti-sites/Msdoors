e20d5da59e665a4885752e176852a6c6718b923e
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-12-25 23:05:00 -0300

    Update Fools23.lua

--- Src/Loaders/Doors/Fools23.lua
+++ Src/Loaders/Doors/Fools23.lua
@@ -22,16 +22,29 @@ local OrionLib = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com
 local Window = OrionLib:MakeWindow({IntroText = "Msdoors | V1",Icon = "rbxassetid://100573561401335", IntroIcon = "rbxassetid://95869322194132", Name = "MsDoors | Super Hard Mode", HidePremium = false, SaveConfig = true, ConfigFolder = ".msdoors/places/fools23"})
 
 --// APIS \\--
---[[ MSDOORS API ]]--
-
 local MsdoorsNotify = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/Notification-doorsAPI/refs/heads/main/Msdoors/MsdoorsApi.lua"))()
---[[ MS ESP(@mstudio45) - thanks for the API! ]]--
 local ESPLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/MS-ESP/refs/heads/main/source.lua"))()
 
 MsdoorsNotify("Msdoors","Msdoors foi carregado com sucesso!","Execução","rbxassetid://133997875469993", Color3.new(0.5, 0, 0.5), 5)
 if game.PlaceId == 6516141723 then
 MsdoorsNotify("Msdoors","Por favor, execute em SUPER HARD MODE.","Execução","rbxassetid://133997875469993", Color3.new(0.5, 0, 0.5), 6)
 end
+
+--// Serviços \\--
+local Lighting = game:GetService("Lighting")
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+local SoundService = game:GetService("SoundService")
+local TextChatService = game:GetService("TextChatService")
+local UserInputService = game:GetService("UserInputService")
+local PathfindingService = game:GetService("PathfindingService")
+local ProximityPromptService = game:GetService("ProximityPromptService")
+local TweenService = game:GetService("TweenService")
+local Workspace = game:GetService("Workspace")
+
+local LocalPlayer = Players.LocalPlayer
+local LatestRoom = ReplicatedStorage:WaitForChild("GameData"):WaitForChild("LatestRoom")
 
 --[[ SCRIPTS ]]--
 --//ANTI BANANA\\--
