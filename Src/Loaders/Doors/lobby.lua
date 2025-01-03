8b56a86b0cb3a0810557dd5ea1841c9f93a3dadd
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-01-03 12:30:58 -0300

    Update lobby.lua

--- Src/Loaders/Doors/lobby.lua
+++ Src/Loaders/Doors/lobby.lua
@@ -18,7 +18,7 @@
 
 --[[ APIS E SISTEMAS]]--
 ---[[ loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/Msdoors/refs/heads/main/Src/Loaders/Doors/test/CustomNameWithGpt.lua"))() ]]--
-local OrionLib = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Giangplay/Script/main/Orion_Library_PE_V2.lua'))()
+local OrionLib = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Sc-Rhyan57/Msdoors/refs/heads/main/Library/OrionLibrary_msdoors.lua'))()
 local Window = OrionLib:MakeWindow({IntroText = "Msdoors | V1",Icon = "rbxassetid://100573561401335", IntroIcon = "rbxassetid://95869322194132", Name = "MsDoors | Lobby", HidePremium = false, SaveConfig = true, ConfigFolder = ".msdoors/places/lobby"})
 local MsdoorsNotify = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/Notification-doorsAPI/refs/heads/main/Msdoors/MsdoorsApi.lua"))()
 
