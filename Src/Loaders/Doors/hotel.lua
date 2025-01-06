d1702e1ef570b8f0fadb52dc9bf0fd7e47f1826e
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-01-06 13:51:49 -0300

    Update hotel.lua

--- Src/Loaders/Doors/hotel.lua
+++ Src/Loaders/Doors/hotel.lua
@@ -20,7 +20,6 @@ if _G.OrionLibLoaded then
     warn("[Msdoors] • Script já está carregado!")
     return
 end
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 local OrionLib = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Sc-Rhyan57/Msdoors/refs/heads/main/Library/OrionLibrary_msdoors.lua'))()
 local Window = OrionLib:MakeWindow({IntroText = "Msdoors | V1",Icon = "rbxassetid://100573561401335", IntroIcon = "rbxassetid://95869322194132", Name = "MsDoors | The Hotel", HidePremium = false, SaveConfig = true, ConfigFolder = ".msdoors/places/hotel"})
 local MsdoorsNotify = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/Notification-doorsAPI/refs/heads/main/Msdoors/MsdoorsApi.lua"))()
